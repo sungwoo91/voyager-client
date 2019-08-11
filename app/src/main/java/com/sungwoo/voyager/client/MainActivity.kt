394ee1b8a453e179a59c49c3f06f9004a0b37aff
leesungwoo <tols3319@gmail.com>
2019-08-11 20:50:55 +0900

    migrate to androidx and add fcm dependency

--- app/src/main/java/com/sungwoo/voyager/client/MainActivity.kt
+++ app/src/main/java/com/sungwoo/voyager/client/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.sungwoo.voyager.client
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
