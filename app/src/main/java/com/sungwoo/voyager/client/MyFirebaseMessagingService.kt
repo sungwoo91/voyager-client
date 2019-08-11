72a1eeb06d7d338fec3a3d2d7ff121da3a379c5c
leesungwoo <tols3319@gmail.com>
2019-08-11 21:16:01 +0900

    firebase 콘솔에서 보낸 메시지 로그로 찍기 구현

--- app/src/main/java/com/sungwoo/voyager/client/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sungwoo/voyager/client/MyFirebaseMessagingService.kt
@@ -1,1 +1,70 @@
 package com.sungwoo.voyager.client
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+        Log.d("FirebaseMessaging", "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+
+    }
+
+    private fun scheduleJob() {
+        // [START dispatch_job]
+//        val work = OneTimeWorkRequest.Builder(MyWorker::class.java).build()
+//        WorkManager.getInstance().beginWith(work).enqueue()
+        // [END dispatch_job]
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private fun handleNow() {
+        Log.d("FirebaseMessaging", "Short lived task is done.")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d("FirebaseMessaging", "From: ${remoteMessage?.from}")
+
+        // Check if message contains a data payload.
+        remoteMessage?.data?.isNotEmpty()?.let {
+            Log.d("FirebaseMessaging", "Message data payload: " + remoteMessage.data)
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+                handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d("FirebaseMessaging", "Message Notification Body: ${it.body}")
+
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+}
